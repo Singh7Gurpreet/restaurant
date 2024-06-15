2b44eb1f93e873e4a938d32d13a30076885a0746
Gurpreet Singh <parasrockz81@gmail.com>
2024-06-15 12:14:54 -0600

    Making my add to cart things work :)

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -3,11 +3,21 @@ const router = express.Router();
 const path = require('path');
 const {getFood} = require('../models/food/foodData')
 
-router.get('/',async (req,res,next)=>{
+router.get('/',async (req,res,next) => {
     const foodItems = await getFood();
     res.render(path.join(__dirname,'../views/public/food.ejs'),{
         data:foodItems
     });
+});
+
+router.post('/cart',async (req,res,next) => {
+    console.log(req.body);
+    res.sendStatus(200);
+});
+
+router.get('/cart',(req,res)=>{
+    console.log("fdss");
+    res.sendStatus(200);
 });
 
 module.exports = router;
--- views/public/scripts/foodCart.js
+++ views/public/scripts/foodCart.js
@@ -1,8 +1,17 @@
-const anchorTag = document.querySelectorAll('a');
+const anchorTag = document.querySelectorAll('.txt');
+const img = document.querySelector('.main_header--cart');
+
+img.addEventListener('click',(event) => {
+    axios.post('./order/cart',{
+        items:sessionStorage.getItem("cart")
+    })
+    window.location.href = "./order/cart";
+})
 
 const cart = [];
 
 
+//can be imporved by sending all data together at once instead of one by one
 for(let i = 0; i < anchorTag.length;i++) {
     anchorTag[i].addEventListener('click',(event)=> {
         cart.push(anchorTag[i].dataset.foodid);
